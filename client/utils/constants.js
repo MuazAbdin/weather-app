54be2efefb94a8ea1cbfdb6cf73935e4c9a2d8d3
Muaz Abdin <muathabd25@gmail.com>
2024-01-10 17:20:30 +0200

    update BASE_URL

--- client/utils/constants.js
+++ client/utils/constants.js
@@ -5,5 +5,5 @@ const DOM_ELEMENTS = {
   homeCity: ".home-city",
 };
 
-const BASE_URL = "http://localhost:3000/weather/v1/cities";
-// const BASE_URL = "https://weather-app-cqp0.onrender.com/weather/v1/cities";
+// const BASE_URL = "http://localhost:3000/weather/v1/cities";
+const BASE_URL = "https://weather-app-cqp0.onrender.com/weather/v1/cities";
